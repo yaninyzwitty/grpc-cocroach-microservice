337ef41dd0106e78321c591c8087d242c4e32e55
yaninyzwitty <ianmwa143@gmail.com>
2025-02-04 17:04:28 +0300

    add product controller methods

---
+++ controller/product_controller.go
@@ -0,0 +1,40 @@
+package controller
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/yaninyzwitty/grpc-cocroach-microservice/pb"
+)
+
+type productController struct {
+	pool *pgxpool.Pool
+	pb.UnimplementedProductServiceServer
+}
+
+// NewProductController returns an instance that implements pb.ProductServiceServer.
+func NewProductController(pool *pgxpool.Pool) pb.ProductServiceServer {
+	return &productController{
+		pool: pool,
+	}
+}
+
+func (c *productController) CreateProduct(ctx context.Context, req *pb.CreateProductRequest) (*pb.CreateProductResponse, error) {
+	return &pb.CreateProductResponse{}, nil
+}
+
+func (c *productController) UpdateProduct(ctx context.Context, req *pb.UpdateProductRequest) (*pb.UpdateProductResponse, error) {
+	return &pb.UpdateProductResponse{}, nil
+}
+
+func (c *productController) DeleteProduct(ctx context.Context, req *pb.DeleteProductRequest) (*pb.DeleteProductResponse, error) {
+	return &pb.DeleteProductResponse{}, nil
+}
+
+func (c *productController) GetProduct(ctx context.Context, req *pb.GetProductRequest) (*pb.GetProductResponse, error) {
+	return &pb.GetProductResponse{}, nil
+}
+
+func (c *productController) ListProducts(ctx context.Context, req *pb.ListProductsRequest) (*pb.ListProductsResponse, error) {
+	return &pb.ListProductsResponse{}, nil
+}
